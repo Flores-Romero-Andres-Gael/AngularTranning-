b2db3ac07c8dc136b3b9f60b9060095798adb1d6
Gael Flores <florogael@gmail.com>
2024-02-27 11:39:44 -0600

    Actualizado 27/02

--- Practica_1/src/app/app.component.ts
+++ Practica_1/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonaComponent } from './persona/persona.component';
+import { PersonasComponent } from './personas/personas.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonaComponent, PersonasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'Practica_1';
+  title = 'Mundo!!';
 }
